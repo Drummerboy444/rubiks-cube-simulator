adb3e800b28ac648a31887d70ad1bbc516f407fa
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-06-04 17:06:28 +0100

    Implementation of Front and Down moves and tweaks to camera

--- Rubik's Cube/Assets/Scripts/CubeController.cs
+++ Rubik's Cube/Assets/Scripts/CubeController.cs
@@ -50,12 +50,20 @@ public class CubeController : MonoBehaviour
 
     private void Front()
     {
-        
+        CycleColours("F1", "F3", "F9", "F7");
+        CycleColours("F2", "F6", "F8", "F4");
+        CycleColours("U7", "R1", "D3", "L9");
+        CycleColours("U8", "R4", "D2", "L6");
+        CycleColours("U9", "R7", "D1", "L3");
     }
 
     private void Down()
     {
-        
+        CycleColours("D1", "D3", "D9", "D7");
+        CycleColours("D2", "D6", "D8", "D4");
+        CycleColours("F7", "R7", "B7", "L7");
+        CycleColours("F8", "R8", "B8", "L8");
+        CycleColours("F9", "R9", "B9", "L9");
     }
 
     private void Left()
