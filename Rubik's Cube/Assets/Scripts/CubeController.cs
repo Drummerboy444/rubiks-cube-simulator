d3ff5865da83f3219c6ea2b70874fcd96f6eadd4
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-06-04 16:53:28 +0100

    Implementationof Up move

--- Rubik's Cube/Assets/Scripts/CubeController.cs
+++ Rubik's Cube/Assets/Scripts/CubeController.cs
@@ -15,6 +15,11 @@ public class CubeController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U)) Up();
+    }
+
     private Colour GetStartingColour(string label)
     {
         return label[0] switch
@@ -27,5 +32,31 @@ public class CubeController : MonoBehaviour
             'B' => Colour.White,
             _ => default
         };
+    }
+
+    private void Up()
+    {
+        CycleColours("U1", "U3", "U9", "U7");
+        CycleColours("U2", "U6", "U8", "U4");
+        CycleColours("F1", "L1", "B1", "R1");
+        CycleColours("F2", "L2", "B2", "R2");
+        CycleColours("F3", "L3", "B3", "R3");
+    }
+
+    private void CycleColours(params string[] labels)
+    {
+        Dictionary<Face, Colour> faceToNewColourLookup = new Dictionary<Face, Colour>();
+
+        for (int i = 0; i < labels.Length; i++)
+        {
+            Face face1 = labelToFaceLookup[labels[i]];
+            Face face2 = labelToFaceLookup[labels[(i + 1) % labels.Length]];
+            faceToNewColourLookup[face2] = face1.Colour;
+        }
+        
+        foreach (KeyValuePair<Face,Colour> kvp in faceToNewColourLookup)
+        {
+            kvp.Key.Colour = kvp.Value;
+        }
     }
 }
